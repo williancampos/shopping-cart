4e23551b7d9ddb569673cdf3c7d338177d565dbc
Willian <willian.campos@gmail.com>
2017-04-25 22:14:06 -0400

    Create WebServer class (SpringBoot application entry point)

---
+++ src/main/java/com/williancampos/shoppingcart/server/WebServer.java
@@ -0,0 +1,16 @@
+package com.williancampos.shoppingcart.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.williancampos.shoppingcart.resource", "com.williancampos.shoppingcart.service" })
+@EnableMongoRepositories(basePackages = { "com.williancampos.shoppingcart.repository" })
+public class WebServer {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(WebServer.class, args);
+	}
+}
