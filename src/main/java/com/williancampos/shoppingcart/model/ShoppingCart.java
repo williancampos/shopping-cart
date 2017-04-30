ad06dc11d99009e2bec5fc84311ed8ac76fae499
Willian <willian.campos@gmail.com>
2017-04-30 10:26:05 -0400

    Add implementation to ShoppingCart and ShoppingCartItem model classes

--- src/main/java/com/williancampos/shoppingcart/model/ShoppingCart.java
+++ src/main/java/com/williancampos/shoppingcart/model/ShoppingCart.java
@@ -1,6 +1,14 @@
 package com.williancampos.shoppingcart.model;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+
+import com.google.common.base.MoreObjects;
 
 /**
  * Representation of the shopping cart. A customer can add and remove products
@@ -9,7 +17,12 @@ import java.util.Set;
  * @author Willian Campos
  *
  */
-public interface ShoppingCart {
+public class ShoppingCart implements Serializable {
+
+	private static final long serialVersionUID = 6504467589777675910L;
+
+	private final Set<ShoppingCartItem> items = new HashSet<>();
+	private final Map<Product, Integer> itemMap = new HashMap<>();
 
 	/**
 	 * Returns the {@link ShoppingCartItem}s currently in the shopping cart.
@@ -17,7 +30,9 @@ public interface ShoppingCart {
 	 * @return the {@link ShoppingCartItem}s
 	 * @see ShoppingCartItem
 	 */
-	Set<ShoppingCartItem> getItems();
+	public Set<ShoppingCartItem> getItems() {
+		return Collections.unmodifiableSet(items);
+	}
 
 	/**
 	 * Add into the cart the specified quantity of a {@link Product}. If the
@@ -29,7 +44,17 @@ public interface ShoppingCart {
 	 * @param quantity
 	 *            the quantity to be added
 	 */
-	void add(Product product, int quantity);
+	public void add(final Product product, final int quantity) {
+		Integer itemQuantity = itemMap.get(product);
+		if (itemQuantity == null) {
+			itemQuantity = 0;
+		}
+		itemQuantity += quantity;
+		itemMap.put(product, itemQuantity);
+		final ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, itemQuantity);
+		items.remove(shoppingCartItem);
+		items.add(shoppingCartItem);
+	}
 
 	/**
 	 * Remove from the cart the specified quantity of a {@link Product}. If the
@@ -44,7 +69,22 @@ public interface ShoppingCart {
 	 * 
 	 * @throws IllegalArgumentException
 	 */
-	void remove(Product product, int quantity);
+	public void remove(final Product product, final int quantity) {
+		Integer itemQuantity = itemMap.get(product);
+		if (itemQuantity == null) {
+			itemQuantity = 0;
+		}
+		itemQuantity -= quantity;
+		final ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, itemQuantity);
+		items.remove(shoppingCartItem);
+		
+		if (itemQuantity > 0) {
+			itemMap.put(product, itemQuantity);
+			items.add(shoppingCartItem);	
+		} else {
+			itemMap.remove(product);
+		}
+	}
 
 	/**
 	 * Update the cart with the specified quantity of a {@link Product}. If this
@@ -57,6 +97,38 @@ public interface ShoppingCart {
 	 * @param quantity
 	 *            the desired quantity
 	 */
-	void update(Product product, int quantity);
+	public void update(final Product product, final int quantity) {
+		final ShoppingCartItem shoppingCartItem = new ShoppingCartItem(product, quantity);
+		items.remove(shoppingCartItem);
+		
+		if (quantity > 0) {
+			itemMap.put(product, quantity);
+			items.add(shoppingCartItem);	
+		} else {
+			itemMap.remove(product);
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(items);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final ShoppingCart other = (ShoppingCart) obj;
+		return Objects.equals(items, other.items);
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this).add("items", items).toString();
+	}
 
 }
--- src/main/java/com/williancampos/shoppingcart/model/ShoppingCartItem.java
+++ src/main/java/com/williancampos/shoppingcart/model/ShoppingCartItem.java
@@ -1,25 +1,87 @@
 package com.williancampos.shoppingcart.model;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.google.common.base.MoreObjects;
+
 /**
- * Represents the association between a {@link Product} and a {@link ShoppingCart}.
- * In other words, it is an abstraction of the quantity of products in a shopping cart.
+ * Represents the association between a {@link Product} and a
+ * {@link ShoppingCart}. In other words, it is an abstraction of the quantity of
+ * products in a shopping cart.
  * 
  * @author Willian Campos
  * @see Product
  * @see ShoppingCart
  *
  */
-public interface ShoppingCartItem {
-	
+public class ShoppingCartItem implements Serializable {
+
+	private static final long serialVersionUID = 7934069585812471894L;
+
+	private Product product;
+	private int quantity;
+
+	public ShoppingCartItem(final Product product, final int quantity) {
+		this.product = product;
+		this.quantity = quantity;
+	}
+
 	/**
 	 * Returns the associated {@link Product}.
+	 * 
 	 * @return the associated {@link Product}
 	 */
-	Product getProduct();
-	
+	public Product getProduct() {
+		return product;
+	}
+
 	/**
 	 * Returns the quantity of the {@link Product}.
+	 * 
 	 * @return the quantity of the {@link Product}
 	 */
-	int getQuantity();
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * 
+	 * @param quantity
+	 */
+	public void addQuantity(final int quantity) {
+		checkArgument(quantity > 0, "The quantity must to be positive.");
+		this.quantity = this.quantity + quantity;
+	}
+
+	public void removeQuantity(final int quantity) {
+		checkArgument(quantity > 0, "The quantity must to be positive.");
+		checkArgument(quantity > this.quantity,
+				"The quantity to be removed can not be higher than the existing quantity.");
+		this.quantity = this.quantity - quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(product);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final ShoppingCartItem other = (ShoppingCartItem) obj;
+		return Objects.equals(product, other.product);
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this).add("product", product).add("quantity", quantity).toString();
+	}
 }
