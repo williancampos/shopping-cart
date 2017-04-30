43797b9af9186e96eee1930a818f09088aa79f0b
Willian <willian.campos@gmail.com>
2017-04-30 10:26:56 -0400

    Add ProductRepository.findByIdentifier

--- src/main/java/com/williancampos/shoppingcart/repository/ProductRepository.java
+++ src/main/java/com/williancampos/shoppingcart/repository/ProductRepository.java
@@ -7,5 +7,7 @@ import com.williancampos.shoppingcart.model.Product;
 
 @Repository
 public interface ProductRepository extends MongoRepository<Product,String> {
+	
+	Product findByIdentifier(final String identifier);
 
 }
